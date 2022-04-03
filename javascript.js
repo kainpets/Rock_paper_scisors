0304fbd76d7f577ec00bcf110d5606224d36ebc2
Lubczyk21 <lubczyk4991@gmail.com>
2022-04-03 12:16:16 +0200

    added else if functionality

--- javascript.js
+++ javascript.js
@@ -2,16 +2,38 @@ let words = ["Rock", "Paper", "Scisors"];
 
 function computerPlay(arr) {
     return arr[Math.floor(Math.random() * words.length)];
-    console.log(computerPlay(words));
+}
+
+console.log(computerPlay(words));
+
+function playRound(arr) {
+    var playerSelection = arr[Math.floor(Math.random() * words.length)];
+    console.log(playerSelection);
+    var computerSelection = arr[Math.floor(Math.random() * words.length)];
+    console.log(computerSelection);
+    
+    if (playerSelection === "Rock" && computerSelection === "Scisors") {
+        console.log("You won! Rock beats Scisors!");
+    } else if (playerSelection === "Rock" && computerSelection === "Paper"){
+        console.log("You lost! Paper beats Rock!");
+    } else if (playerSelection === "Rock" && computerSelection === "Rock"){
+        console.log("It is a draw!");
+    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
+        console.log("You won! Paper beats Rock!");
+    } else if (playerSelection === "Paper" && computerSelection === "Paper"){
+        console.log("It is a draw!");
+    } else if (playerSelection === "Paper" && computerSelection === "Scisors"){
+        console.log("You lost! Paper beats Scisors!");
+    } else if (playerSelection === "Scisors" && computerSelection === "Rock"){
+        console.log("You lost! Rock beats Scisors!");
+    } else if (playerSelection === "Scisors" && computerSelection === "Paper"){
+        console.log("You won! Scisors beats Paper!");
+    } else if (playerSelection === "Scisors" && computerSelection === "Scisors"){
+        console.log("It is a draw!");
+    }
+ 
 }
 
 
-    
 
-function playerSelection(arr) {
-    return arr[Math.floor(Math.random() * words.length)];
-    }
-    
-for (let x=0; x<1; x++)
-    console.log(playerSelection(words));
-
+console.log(playRound(words));
