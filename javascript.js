92fc66fe48a6f9ff7f01449c802f5ad71eda556b
kainpets <pawelstepniak22@gmail.com>
2022-04-03 15:47:31 +0200

    proposed changes

--- javascript.js
+++ javascript.js
@@ -1,48 +1,50 @@
-var words = ["Rock", "Paper", "Scisors"];
-
-//function computerPlay(arr) {
-//    return arr[Math.floor(Math.random() * words.length)];
-//}
-
-function computerPlay(arr) {
-    return arr[Math.floor(Math.random() * words.length)];
-}
-
-console.log(computerPlay(words));
-
 function playRound(playerSelection, computerSelection) {
-    var playerNumber = [Math.floor(Math.random() * 3)];
-    var playerSelection = words[playerNumber];
-    console.log(playerSelection);
-    var computerNumber = [Math.floor(Math.random() * 3)];
-    var computerSelection = words[computerNumber]
-    console.log(computerSelection);
-    
-    if (playerSelection === "Rock" && computerSelection === "Scisors") {
-        console.log("You won! Rock beats Scisors!");
-    } else if (playerSelection === "Rock" && computerSelection === "Paper"){
-        console.log("You lost! Paper beats Rock!");
-    } else if (playerSelection === "Rock" && computerSelection === "Rock"){
+    // dodaj wiadomość do gracza, żeby wiedział o co chodzi
+    // wiadomość wykorzystuje string interpolation - wygoogluj "mdn template literals", jesli nie znasz tego
+    const messageToPlayer = `Computer chose ${computerSelection}, you chose ${playerSelection}`;
+    console.log(messageToPlayer);
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("You won! rock beats scissors!");
+    } else if (playerSelection === "rock" && computerSelection === "paper"){
+        console.log("You lost! paper beats rock!");
+    } else if (playerSelection === "rock" && computerSelection === "rock"){
         console.log("It is a draw!");
-    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
-        console.log("You won! Paper beats Rock!");
-    } else if (playerSelection === "Paper" && computerSelection === "Paper"){
+    } else if (playerSelection === "paper" && computerSelection === "rock"){
+        console.log("You won! paper beats rock!");
+    } else if (playerSelection === "paper" && computerSelection === "paper"){
         console.log("It is a draw!");
-    } else if (playerSelection === "Paper" && computerSelection === "Scisors"){
-        console.log("You lost! Paper beats Scisors!");
-    } else if (playerSelection === "Scisors" && computerSelection === "Rock"){
-        console.log("You lost! Rock beats Scisors!");
-    } else if (playerSelection === "Scisors" && computerSelection === "Paper"){
-        console.log("You won! Scisors beats Paper!");
-    } else if (playerSelection === "Scisors" && computerSelection === "Scisors"){
+    } else if (playerSelection === "paper" && computerSelection === "scissors"){
+        console.log("You lost! paper beats scissors!");
+    } else if (playerSelection === "scissors" && computerSelection === "rock"){
+        console.log("You lost! rock beats scissors!");
+    } else if (playerSelection === "scissors" && computerSelection === "paper"){
+        console.log("You won! scissors beats paper!");
+    } else if (playerSelection === "scissors" && computerSelection === "scissors"){
         console.log("It is a draw!");
     }
- 
 }
 
-console.log(playRound(words));
+// computerPlay returns "rock||paper||scissors" based on random number
+function computerPlay() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    // zwróć rock/paper/scissors na podstawie randomowej liczby od 0-2
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
 
-const playerSelection = "Rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+// playerPlay returns "rock||paper||scissors" from user's input
+function playerPlay() {
+    // uzyj prompt(), żeby dostać input od gracza
+    const userInput = prompt("rock, paper, scissors: ");
+    return userInput;
+}
+
+// do playRound() przesyłasz dwa parametry (w tym wypadku to funkcje), za które podstawiają się wartości, które one zwracają w "return"
+console.log(playRound(playerPlay(), computerPlay()));
 
